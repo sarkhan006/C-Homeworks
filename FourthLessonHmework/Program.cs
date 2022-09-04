909a34dc19e3e2e33018eb1c200d2c4158e41594
Sarkhan <sarkhan.eyn@gmail.com>
2022-09-04 21:30:13 +0200

    4lesson 2 homework is made

--- FourthLessonHmework/Program.cs
+++ FourthLessonHmework/Program.cs
@@ -3,7 +3,7 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-
+/*
 int Exponentiation(int numberA, int numberB){
   int result = 1;
   for(int i=1; i <= numberB; i++){
@@ -20,3 +20,29 @@ int Exponentiation(int numberA, int numberB){
 
   int exponentiation = Exponentiation(numberA, numberB);
   Console.WriteLine("Answer is1 " + exponentiation);
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Provide number N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("The sum of digits is : " + sumNumber);
