5fa26ffe714c1cfdc27921cbfa24ce5891a2c0e7
Sarkhan <sarkhan.eyn@gmail.com>
2022-09-15 23:42:13 +0200

    fifth lesson 2 task is made

--- FifthLessonHomework/Program.cs
+++ FifthLessonHomework/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-
+/*
 int[] numbers = new int[10];
 
 void FillArray(int[] array, int min, int max){
@@ -35,3 +35,37 @@ Console.WriteLine();
 int quantity = QuantityPositive(numbers);
 Console.WriteLine($"The amount of numbers that /2 without rest is: {quantity}");
 
+*/      
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+// Console.Clear();
+
+Console.Write($"Input the quantity of numbers: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNumbers(int numberElements, int min, int max)
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("The random numbers picked by the sistem are: ");
+
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumbers[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+  return sumElements;
+  }
+
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+
+Console.WriteLine($"\nThe sum of the elements that don't /2 without the rest is: {randomNumbers}");
