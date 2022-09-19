c2016da3624f4525ed895458adf1cf2880d0019a
Sarkhan <sarkhan.eyn@gmail.com>
2022-09-19 22:50:27 +0200

    six lesson 1 task is made

---
+++ SixLessonHomework/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+
+Console.Write($"Please input number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Input {i+1} number: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"The amount of numbers bigger than 0  : {Comparison(massiveNumbers)} ");
