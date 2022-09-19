a5d77568373e26e053f3e6b45d5a0b4e2807ce78
Sarkhan <sarkhan.eyn@gmail.com>
2022-09-19 23:16:21 +0200

    7 lesson 3 task is made

--- SeventhLessonHomework/Program.cs
+++ SeventhLessonHomework/Program.cs
@@ -56,7 +56,7 @@ for (int i = 0; i < m; i++)
 // 17 -> такого числа в массиве нет
 
 
-
+/*
 
 Console.Write("Please input the coordinates of the position of element divided by ' , ': ");
 Console.Write("\nLets take the array in the task (№ 47).\n");
@@ -116,4 +116,55 @@ string RemovingSpaces (string input)
     }
   }
   return output;
+}
+*/
+
+
+// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+
+int[,] arrayWhole = new int[m, n];
+arrayWhole = TransformationArrayWhole(array);
+
+WriteArrayInt(arrayWhole);
+
+Console.Write($"\nThe average is:\n");
+for (int i = 0; i < n; i++)
+{
+  double arithmeticMean = 0;
+  for (int j = 0; j < m; j++)
+  {
+    arithmeticMean += arrayWhole[j, i];
+  }
+  arithmeticMean = Math.Round(arithmeticMean / m, 1);
+  Console.WriteLine($"Column № {i+1} {arithmeticMean}");
+}
+
+int[,] TransformationArrayWhole (double[,] array)
+{
+  int[,] arrayWhole = new int[array.GetLength(0), array.GetLength(1)];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      arrayWhole[i, j] = Convert.ToInt32(array[i, j]);
+    }
+  }
+  return arrayWhole;
+}
+
+void WriteArrayInt (int[,] arrayWhole){
+for (int i = 0; i < m; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+        Console.Write(arrayWhole[i, j] + " ");
+      }
+      Console.WriteLine();
+  }
 }
