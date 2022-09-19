e09afc1588da8e5afc42084f39df179f25b54794
Sarkhan <sarkhan.eyn@gmail.com>
2022-09-19 22:55:15 +0200

    six lesson 2 task is made

--- SixLessonHomework/Program.cs
+++ SixLessonHomework/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
-
+/*
 Console.Clear();
 
 
@@ -31,3 +31,53 @@ int Comparison(int[] massiveNumbers)
 InputNumbers(m);
 
 Console.WriteLine($"The amount of numbers bigger than 0  : {Comparison(massiveNumbers)} ");
+*/
+
+// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+
+
+
+double[,] coeff = new double[2, 2];
+double[] crossPoint = new double[2];
+
+void InputCoefficients(){
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Input the coef. of {i+1}- equation (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Input the coef. k: ");
+      else Console.Write($"Input coef. b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Decision(double[,] coeff)
+{
+  crossPoint[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  crossPoint[1] = crossPoint[0] * coeff[0,0] + coeff[0,1];
+  return crossPoint;
+}
+
+void OutputResponse(double[,] coeff)
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.Write($"\nThe lines are matching");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.Write($"\nThe lines are parallel");
+  }
+  else 
+  {
+    Decision(coeff);
+    Console.Write($"\nThe place of matching of lines: ({crossPoint[0]}, {crossPoint[1]})");
+  }
+}
+
+InputCoefficients();
+OutputResponse(coeff);
